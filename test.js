c667ed78fcb8dcdc7a23966f21d376f99f198a99
TubacVlad <vladtubac@gmail.com>
2024-10-08 16:19:58 +0300

    variables

---
+++ test.js
@@ -0,0 +1,10 @@
+function test(){
+    if (true){
+        var var1 = 1;
+        const const1 = 2;
+        let let3 = 3;
+    }
+    console.log(var1);
+    console.log(const1);
+    console.log(let3);
+}
